1ecadfb2d527a494f527dd9e2b2499397020bfe0
JongGeun Lee <johnbr1212@naver.com>
2021-05-07 14:49:27 +0900

    GAN commit

---
+++ GAN/class_loss.py
@@ -0,0 +1,15 @@
+class Loss:
+    def __init__(self):
+        self.initVariable = 1
+
+    def lossBaseFunction(self):
+
+        return
+    
+    def lossBaseFunction2(self):
+        
+        return
+    
+    def lossBaseFunction3(self):
+
+        return
