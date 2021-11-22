07e8f0878865bb785633c700ce4afa2af26a2153
JongGeun Lee <johnbr1212@naver.com>
2021-11-22 17:34:22 +0900

    Assignment4

---
+++ Assignment4/hw4_nn.py
@@ -0,0 +1,306 @@
+import numpy as np
+from skimage.util.shape import view_as_windows
+
+
+#######
+# if necessary, you can define additional functions which help your implementation,
+# and import proper libraries for those functions.
+#######
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+        """다수의 이미지를 입력받아 2차원 배열로 변환한다(평탄화).
+        
+        Parameters
+        ----------
+        input_data : 4차원 배열 형태의 입력 데이터(이미지 수, 채널 수, 높이, 너비)
+        filter_h : 필터의 높이
+        filter_w : 필터의 너비
+        stride : 스트라이드
+        pad : 패딩
+        
+        Returns
+        -------
+        col : 2차원 배열
+        """
+        N, C, H, W = input_data.shape
+        out_h = (H + 2*pad - filter_h)//stride + 1
+        out_w = (W + 2*pad - filter_w)//stride + 1
+
+        img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+        col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+        for y in range(filter_h):
+            y_max = y + stride*out_h
+            for x in range(filter_w):
+                x_max = x + stride*out_w
+                col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+        col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+        return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """(im2col과 반대) 2차원 배열을 입력받아 다수의 이미지 묶음으로 변환한다.
+    
+    Parameters
+    ----------
+    col : 2차원 배열(입력 데이터)
+    input_shape : 원래 이미지 데이터의 형상（예：(10, 1, 28, 28)）
+    filter_h : 필터의 높이
+    filter_w : 필터의 너비
+    stride : 스트라이드
+    pad : 패딩
+    
+    Returns
+    -------
+    img : 변환된 이미지들
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
+
+class nn_convolutional_layer:
+
+    def __init__(self, filter_width, filter_height, input_size, in_ch_size, num_filters, std=1e0):
+        # initialization of weights
+        self.W = np.random.normal(0, std / np.sqrt(in_ch_size * filter_width * filter_height / 2),
+                                  (num_filters, in_ch_size, filter_width, filter_height))
+        self.b = 0.01 + np.zeros((1, num_filters, 1, 1))
+        self.input_size = input_size
+        self.size = input_size - filter_weight + 1
+        self.pad = 0
+        self.stride = 1
+        #######
+        ## If necessary, you can define additional class variables here
+        #######
+
+    def update_weights(self, dW, db):
+        self.W += dW
+        self.b += db
+
+    def get_weights(self):
+        return self.W, self.b
+
+    def set_weights(self, W, b):
+        self.W = W
+        self.b = b
+
+    #######
+    # Q1. Complete this method
+    #######
+    def forward(self,x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + (H - FH)
+        out_w = 1 + (W - FW)
+
+        col = im2col(x,FH,FW,self.stride,self.pad)
+        col_W = self.W.reshape(FN,-1).T
+        out = np.dot(col,col_W) + self.b
+        out = out.reshape(N,out_h,out_w,-1).transpose(0,3,1,2)
+
+        return out
+    # output size = (N-F) + 1
+
+    #######
+    # Q2. Complete this method
+    #######
+    def backprop(self, x, dLdy):
+        return dLdx, dLdW, dLdb
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dLdx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+    #######
+    ## If necessary, you can define additional class methods here
+    #######
+
+class nn_max_pooling_layer:
+    def __init__(self, stride, pool_size):
+        self.stride = stride
+        self.pool_size = pool_size
+        #######
+        ## If necessary, you can define additional class variables here
+        #######
+        self.pad = 0
+
+    #######
+    # Q3. Complete this method
+    #######
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_size) / self.stride)
+        out_w = int(1 + (W - self.pool_size) / self.stride)
+
+        col = im2col(x, self.pool_size, self.pool_size, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_size*self.pool_size)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    #######
+    # Q4. Complete this method
+    #######
+    def backprop(self, x, dLdy):
+        return dLdx
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_size * self.pool_size
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dLdx = col2im(dcol, self.x.shape, self.pool_size, self.pool_size, self.stride, self.pad)
+        
+        return dLdx
+
+    #######
+    ## If necessary, you can define additional class methods here
+    #######
+    
+
+# testing the implementation
+
+# data sizes
+batch_size = 8
+input_size = 32
+filter_width = 3
+filter_height = filter_width
+in_ch_size = 3
+num_filters = 8
+
+std = 1e0
+dt = 1e-3
+
+# number of test loops
+num_test = 20
+
+# error parameters
+err_dLdb = 0
+err_dLdx = 0
+err_dLdW = 0
+err_dLdx_pool = 0
+
+for i in range(num_test):
+    # create convolutional layer object
+    cnv = nn_convolutional_layer(filter_width, filter_height, input_size, in_ch_size, num_filters, std)
+
+    x = np.random.normal(0, 1, (batch_size, in_ch_size, input_size, input_size))
+    delta = np.random.normal(0, 1, (batch_size, in_ch_size, input_size, input_size)) * dt
+
+    # dLdx test
+    print('dLdx test')
+    y1 = cnv.forward(x)
+    y2 = cnv.forward(x + delta)
+
+    bp, _, _ = cnv.backprop(x, np.ones(y1.shape))
+
+    exact_dx = np.sum(y2 - y1) / dt
+    apprx_dx = np.sum(delta * bp) / dt
+    print('exact change', exact_dx)
+    print('apprx change', apprx_dx)
+
+    err_dLdx += abs((apprx_dx - exact_dx) / exact_dx) / num_test * 100
+
+    # dLdW test
+    print('dLdW test')
+    W, b = cnv.get_weights()
+    dW = np.random.normal(0, 1, W.shape) * dt
+    db = np.zeros(b.shape)
+
+    z1 = cnv.forward(x)
+    _, bpw, _ = cnv.backprop(x, np.ones(z1.shape))
+    cnv.update_weights(dW, db)
+    z2 = cnv.forward(x)
+
+    exact_dW = np.sum(z2 - z1) / dt
+    apprx_dW = np.sum(dW * bpw) / dt
+    print('exact change', exact_dW)
+    print('apprx change', apprx_dW)
+
+    err_dLdW += abs((apprx_dW - exact_dW) / exact_dW) / num_test * 100
+
+    # dLdb test
+    print('dLdb test')
+
+    W, b = cnv.get_weights()
+
+    dW = np.zeros(W.shape)
+    db = np.random.normal(0, 1, b.shape) * dt
+
+    z1 = cnv.forward(x)
+
+    V = np.random.normal(0, 1, z1.shape)
+
+    _, _, bpb = cnv.backprop(x, V)
+
+    cnv.update_weights(dW, db)
+    z2 = cnv.forward(x)
+
+    exact_db = np.sum(V * (z2 - z1) / dt)
+    apprx_db = np.sum(db * bpb) / dt
+
+    print('exact change', exact_db)
+    print('apprx change', apprx_db)
+    err_dLdb += abs((apprx_db - exact_db) / exact_db) / num_test * 100
+
+    # max pooling test
+    # parameters for max pooling
+    stride = 2
+    pool_size = 2
+
+    mpl = nn_max_pooling_layer(stride=stride, pool_size=pool_size)
+
+    x = np.arange(batch_size * in_ch_size * input_size * input_size).reshape(
+        (batch_size, in_ch_size, input_size, input_size)) + 1
+    delta = np.random.normal(0, 1, (batch_size, in_ch_size, input_size, input_size)) * dt
+
+    print('dLdx test for pooling')
+    y1 = mpl.forward(x)
+    dLdy = np.random.normal(0, 10, y1.shape)
+    bpm = mpl.backprop(x, dLdy)
+
+    y2 = mpl.forward(x + delta)
+
+    exact_dx_pool = np.sum(dLdy * (y2 - y1)) / dt
+    apprx_dx_pool = np.sum(delta * bpm) / dt
+    print('exact change', exact_dx_pool)
+    print('apprx change', apprx_dx_pool)
+
+    err_dLdx_pool += abs((apprx_dx_pool - exact_dx_pool) / exact_dx_pool) / num_test * 100
+
+# reporting accuracy results.
+print('accuracy results')
+print('conv layer dLdx', 100 - err_dLdx, '%')
+print('conv layer dLdW', 100 - err_dLdW, '%')
+print('conv layer dLdb', 100 - err_dLdb, '%')
+print('maxpool layer dLdx', 100 - err_dLdx_pool, '%')
